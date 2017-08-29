80a596a35f8e448ea334bca8e98cd39ed49be578
heartziq <heartziq@gmail.com>
2017-08-29 08:12:40 +0800

    code formatted according to PEP-8 Guideline

--- entertainment_center.py
+++ entertainment_center.py
@@ -1,32 +1,37 @@
-import media, fresh_tomatoes
+import media
+import fresh_tomatoes
 
-toy_story = media.Movie("Toy Story", "A story about a boy named Alan who is obssessed with his new toy",
-                        "https://upload.wikimedia.org/wikipedia/en/1/13/Toy_Story.jpg",
-                        "https://www.youtube.com/watch?v=4KPTXpQehio")
+the_intership = media.Movie("The Internship",
+                            "https://upload.wikimedia.org/wikipedia/en/e/ed/"
+                            "The-internship-poster.jpg",
+                            "https://www.youtube.com/watch?v=cdnoqCViqUo")
 
+batman = media.Movie("Batman: The Dark Knight",
+                     "https://upload.wikimedia.org/wikipedia/en/8/8a/Dark_K"
+                     "night.jpg",
+                     "https://www.youtube.com/watch?v=EXeTwQWrcwY")
 
-avatar = media.Movie("Avatar", "A marine on an alien planet",
-                     "https://upload.wikimedia.org/wikipedia/en/b/b0/Avatar-Teaser-Poster.jpg",
-                     "https://www.youtube.com/watch?v=5PSNL1qE6VY")
+spiderman = media.Movie("Spider-Man: Homecoming",
+                        "https://upload.wikimedia.org/wikipedia/en/f/"
+                        "f9/Spider-Man_Homecoming_poster.jpg",
+                        "https://www.youtube.com/watch?v=U0D3AOldjMU")
 
-school_of_rock = media.Movie("The School Of Rock",
-                             "An aspired rockstar poses as a school teacher",
-                             "https://upload.wikimedia.org/wikipedia/en/1/11/School_of_Rock_Poster.jpg",
-                             "https://www.youtube.com/watch?gl=SG&hl=en-GB&v=XCwy6lW5Ixc")
+guardians_galaxy = media.Movie("Guardians of the Galaxy Vol. 2",
+                               "https://upload.wikimedia.org/wikip"
+                               "edia/en/9/95/GotG_Vol2_poster.jpg",
+                               "https://www.youtube.com/watch?v=2cv2ueYnKjg")
 
-silence_of_the_lambs = media.Movie("Silence Of The Lambs", "A story about a boy named Alan who is obssessed with his new toy",
-                        "https://upload.wikimedia.org/wikipedia/en/8/86/The_Silence_of_the_Lambs_poster.jpg",
-                        "https://www.youtube.com/watch?v=RuX2MQeb8UM")
-
-gone_girl = media.Movie("Gone Girl", "A story about a boy named Alan who is obssessed with his new toy",
-                        "https://upload.wikimedia.org/wikipedia/en/0/05/Gone_Girl_Poster.jpg",
+gone_girl = media.Movie("Gone Girl",
+                        "https://upload.wikimedia.org/wikipedia/en/0/05/Go"
+                        "ne_Girl_Poster.jpg",
                         "https://www.youtube.com/watch?v=Ym3LB0lOJ0o")
 
-nightcrawler = media.Movie("Nightcrawler", "A story about a boy named Alan who is obssessed with his new toy",
-                        "https://upload.wikimedia.org/wikipedia/en/d/d4/Nightcrawlerfilm.jpg",
-                        "https://www.youtube.com/watch?v=u1uP_8VJkDQ")
+nightcrawler = media.Movie("Nightcrawler",
+                           "https://upload.wikimedia.org/wikipedia/en/d/d4/Ni"
+                           "ghtcrawlerfilm.jpg",
+                           "https://www.youtube.com/watch?v=u1uP_8VJkDQ")
 
+movies = [the_intership, batman, spiderman,
+          guardians_galaxy, gone_girl, nightcrawler]
 
-
-movies = [toy_story, avatar, school_of_rock, silence_of_the_lambs, gone_girl, nightcrawler]
 fresh_tomatoes.open_movies_page(movies)
